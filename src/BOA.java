b34567e497fe5befd3293cb1058fb0e27f32171d
Joseph-Ghormley <54871175+Joseph-Ghormley@users.noreply.github.com>
2023-06-29 17:21:34 -0400

    Add files via upload

--- src/BOA.java
+++ src/BOA.java
@@ -1,11 +1,23 @@
 public class BOA implements BankInterface
 {
   @Override
-
   public void calculateInterest()
   {
       System.out.println("The interest rate is 5%");
   }
 
+  @Override
+    public void deposite()
+  {
+      BankInterface.super.deposite();
+      System.out.println(" to BAC");
+  }
+
+  @Override
+    public void withdraw()
+  {
+      BankInterface.super.withdraw();
+      System.out.println(" from BAC");
+  }
 
 }
