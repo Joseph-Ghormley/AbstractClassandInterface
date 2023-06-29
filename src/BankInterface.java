8b1119d41bb67c26b69595471ce0f541e8593d59
Joseph-Ghormley <54871175+Joseph-Ghormley@users.noreply.github.com>
2023-06-29 17:21:26 -0400

    Add files via upload

--- src/BankInterface.java
+++ src/BankInterface.java
@@ -1,16 +1,12 @@
 public interface BankInterface
 {
-    default void BOA(){
-        System.out.println("Bank of America (BOA) TRANSACTION");
-        System.out.println("amount withdraw to BAC");
-        System.out.println("amount deposited BAC");
-    }
-
-    default void Chase_bank()
+   default void withdraw()
     {
-        System.out.println("Chase BANK TRANSACTION");
-        System.out.println("amount withdraw to Chase");
-        System.out.println("amount deposited to Chase");
+        System.out.print("amount withdraw");
+    }
+    default void deposite()
+    {
+        System.out.print("amount deposited");
     }
     void calculateInterest();
 
